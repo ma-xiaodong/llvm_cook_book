52228536f40de516440001bcfe67446b0ab740d8
ma-xiaodong <xdma@163.com>
2020-05-26 16:32:04 +0800

    use seperate dir for each example in chap 4

---
+++ chap4/00_FunCount/build/CMakeFiles/chap4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chap4.dir/FunCount.cpp.o"
+  "libchap4.pdb"
+  "libchap4.so"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chap4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- chap4/00_FunCount/build_run.sh
+++ chap4/00_FunCount/build_run.sh
@@ -1,4 +1,6 @@
 cd ./build
+rm -rf *
+cmake ../
 make
 cd ../
 opt -load ./build/libchap4.so -fc exam_00.ll -disable-output -debug-pass=Structure
